1c931ddb633f74695ce869b4a40e84118b0c6a65
Brian Clymer <crdnilfan@gmail.com>
2018-04-08 21:06:25 -0500

    Persist local storage just to make my testing easier yay.

--- App.tsx
+++ App.tsx
@@ -6,20 +6,32 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './src/redux';
 import { createReduxBoundAddListener, createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const navMiddleware = createReactNavigationReduxMiddleware('root', state => state.nav);
 
 export const addListener = createReduxBoundAddListener('root');
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
 export default class App extends React.Component {
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, navMiddleware));
+    const persistedReducer = persistReducer(persistConfig, reducers);
+    const store = createStore(persistedReducer, {}, applyMiddleware(ReduxThunk, navMiddleware));
+    const persistor = persistStore(store);
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppWithNavigationState />
-        </View>
+        <PersistGate loading={null} persistor={persistor}>
+          <View style={styles.container}>
+            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+            <AppWithNavigationState />
+          </View>
+        </PersistGate>
       </Provider>
     );
   }
